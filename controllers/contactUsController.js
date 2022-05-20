00b5592b0957bde87bb7189cfd69a48bfdd1fa5e
rafdeguzman <1824975@johnabbottcollege.net>
2022-05-20 03:27:00 -0400

    Added Payal's language cookie

--- controllers/contactUsController.js
+++ controllers/contactUsController.js
@@ -8,7 +8,40 @@ const routeRoot = '/';
  * @param {*} res The response object.
  */
 async function showContactUs(req, res){
-    res.render("contact.hbs")
+    let lang = req.cookies.language;
+    let name;
+    let emailAddress;
+    let copyMsg;
+    let send;
+    let technicalSupport;
+    let salesQuestions;
+    let press;
+    let bugReport;
+
+    if (!lang || lang === 'en') {
+        name = "Name"
+        emailAddress = "Email Address"
+        copyMsg = "Send me a copy of this message"
+        send = "Send"
+        technicalSupport = "Technical Support"
+        salesQuestions = "Sales Questions"
+        press = "Press"
+        bugReport = "Bug Report"
+
+    } 
+    else {
+        name = "Nom"
+        emailAddress = "Addresse e-mail"
+        copyMsg = "Envoye-moi une copie de ce message"
+        send = "Envoyer"
+        technicalSupport = "Assistance technique"
+        salesQuestions = "Questions de vente"
+        press = "Presse"
+        bugReport = "rapport de bug"
+
+    }
+
+    res.render("contact.hbs", {name:name, emailAddress:emailAddress, copyMsg:copyMsg, send:send, technicalSupport:technicalSupport, salesQuestions:salesQuestions, press:press, bugReport:bugReport})
 }
 
 router.get('/contact', showContactUs);
@@ -17,4 +50,4 @@ module.exports = {
     showContactUs,
     router,
     routeRoot
-}
+} 
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -14,23 +14,52 @@ async function showHome(req, res){
     let description;
     let recentItems;
     let shop;
+    let currentPromotions;
+    let title1;
+    let message1;
+    let link1;
+    let title2;
+    let message2;
+    let link2;
+    let title3;
+    let message3;
+    let link3;
 
     if (!lang || lang === 'en') {
         current = 'English';
         description = 'shop our newest cameras and more'; // default to English if not already set.
         recentItems = 'recently viewed items'
         shop = "shop now"
+        currentPromotions = "Current Promotions"
+        title1 = "Capture your Memories"
+        message1 = "'Photos allow us to record life's greatest moments, so they can stay in our memories forever.' - Keo Tsang"
+        link1 = "Visit Keo's Channel"
+        title2 = "Premium Camera Hardware"
+        message2 = "Get the latest cameras, lenses and more from us."
+        link2 = "Shop Now"
+        title3 = "The Meaning of Photography"
+        message3 = "'Photography means something to me. What does it mean to you?' - Peter McKinnon"
+        link3 = "Read more"
     } else {
         current = "French"
         description = 'magasinez nos caméras les plus récentes et encore';
-        featItems = 'articles récemment vus'
+        recentItems = 'articles récemment vus'
         shop = "magasinez maintenant"
-
+        currentPromotions = "Promotions En Cours"
+        title1 = "Capturez Vos Souvenirs"
+        message1 = "'Les photos nous permettent d'enregistrer les plus grands moments de la vie, afin qu'ils puissent rester dans nos mémoires pour toujours.' - Kéo Tsang"
+        link1 = "Visitez La Chaîne De Keo"
+        title2 = "Prime Matériel De Caméra"
+        message2 = "Obtenez les derniers appareils photo, objectifs et plus encore de notre part."
+        link2 = "Magasinez Maintenant"
+        title3 = "Le Sens De La Photographie"
+        message3 = "« La photographie signifie quelque chose pour moi. Qu'est-ce que cela signifie pour vous ? » -Peter McKinnon"
+        link3 = "en savoir plus"
     }
 
     const renderItems = {
         recentlyViewed: req.cookies['recentlyViewed'],
-        description:description, recentItems:recentItems, shop: shop
+        description:description, recentItems:recentItems, shop: shop, currentPromotions:currentPromotions, title1:title1, title2:title2, title3:title3, message1:message1, message2:message2, message3:message3, link1:link1, link2:link2, link3:link3
     };
     res.render("home.hbs", renderItems)
 }
